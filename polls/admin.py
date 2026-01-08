b83ddd982e05afb132b16303a0071c9591c0c501
rushdylan-psi <dylan.rush1@gmail.com>
2026-01-08 08:54:28 -0800

    Initial commit: Django polls app

---
+++ polls/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
